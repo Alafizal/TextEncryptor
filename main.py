9d79b50b89537a2df6024a2276a888a8763c6b78
Ala <ala123sweet2006@gmail.com>
2024-04-27 13:25:50 +0530

    outlined basic idea

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print('hello world')
-print('text encryptor')
+# TEXT ENCRYPTOR
+# Authors : Ala Fizal, Mohammed Aghil Puthiyottil
+
+# 1. Get a text as input, for now it can be a sample text (Get a string)
+plain_message = "how are you"
+# 2. We will change it to something else in some-way
+# which people cannot understand plainly
+
+
+# 3. print the encrypted text
